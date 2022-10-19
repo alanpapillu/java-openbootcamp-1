676397a7baae6e968cfa4a14c0b38f1e930a3235
Alan <alan.papillu@hotmail.com>
2022-10-19 16:32:16 -0300

    ejercicio corregido

--- proyecto/ejercicio.java
+++ proyecto/ejercicio.java
@@ -2,7 +2,30 @@ public class ejercicio {
 
     public static void main(String[] args) {
 
-        System.out.println("Tipos de datos: int, long, double, boolean, String");
+        String cadena = "cadena de caracteres";
+
+        //enteros
+        Byte numero1 = 1;
+        Short numero2 = 2;
+        int numero3 = 3;
+        Long numero4 = 400l;
+
+        //decimales
+        float numero5 = 20.2f;
+        double numero6 = 25.23;
+
+        //caracter
+
+        char caracter = 'a';
+
+        //booleano
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println("Tipos de datos en práctica: " + numero1 + ", " + numero2 + ", " + numero3 
+        + ", " + numero4 + ", " + numero5 + ", " + numero6 + ", " + cadena + ", " + caracter + ", " 
+        + verdadero + ", " + falso);
 
     }
 }
